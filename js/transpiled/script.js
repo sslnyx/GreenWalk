cd3ad149d9f9015a13c9f1d19d6a26319428fb7d
sslnyx <sslnyx@gmail.com>
2018-11-05 04:30:35 -0800

    added animation

--- js/transpiled/script.js
+++ js/transpiled/script.js
@@ -3,6 +3,29 @@
 // });
 
 jQuery(document).ready(function ($) {
+
+  // $('.ml6 .letters').each(function(){
+  //   $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+  // });
+
+  // anime.timeline({loop: false})
+  //   .add({
+  //     targets: '.ml6 .letter',
+  //     translateY: ["1.1em", 0],
+  //     translateZ: 0,
+  //     duration: 750,
+  //     delay: function(el, i) {
+  //       return 50 * i;
+  //     }
+  //   }).add({
+  //     targets: '.ml6',
+  //     opacity: 1,
+  //     duration: 1000,
+  //     easing: "easeOutExpo",
+  //     delay: 1000
+  //   });
+
+
   // Select all links with hashes
   $('a[href*="#"]')
   // Remove links that don't actually link to anything
